03323e5c9240b80551f46794d22f97eb4df6c37b
Gabriel Moncea <gabimoncha@gmail.com>
2021-01-21 21:47:52 +0200

    chore: add and mock react-i18next

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'plugin:jest/recommended'],
+  plugins: ['jest'],
+  rules: {
+    curly: 'off',
+  },
 };
--- template/jest.setup.js
+++ template/jest.setup.js
@@ -1,5 +1,8 @@
 import mockRNCNetInfo from '@react-native-community/netinfo/jest/netinfo-mock.js';
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+
 jest.mock('@react-native-community/netinfo', () => mockRNCNetInfo);
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 
 jest.mock('@sentry/react-native', () => ({
   init: jest.fn(),
