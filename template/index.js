0ee37e43cef854e6cdfbf0bf3ecf7421b8a480b1
Daniel Williams <danny.h.williams@hotmail.com>
2021-03-20 21:44:50 +0000

    add storybook
    I've setup something very basic for storybook to help get started

--- template/index.js
+++ template/index.js
@@ -3,5 +3,15 @@ import './wdyr';
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+import getStorybookUI from './storybook';
 
-AppRegistry.registerComponent(appName, () => App);
+let EntryPoint;
+const showStorybook = true;
+
+if (showStorybook && __DEV__) {
+  EntryPoint = getStorybookUI();
+} else {
+  EntryPoint = App;
+}
+
+AppRegistry.registerComponent(appName, () => EntryPoint);
